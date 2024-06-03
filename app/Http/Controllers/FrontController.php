7c9256821cf06fbc4297d9405937eee45168355d
Joreinhard Munandar <jorensaragih@gmail.com>
2024-06-03 12:10:41 +1000

    Initial commit

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    //
+    public function index(){
+        $projects = Project::orderBy('id','desc')->take(6)->get();
+        return view('front.index', [
+            'projects' => $projects
+        ]);
+    }
+
+    public function details(){
+        return view('front.details');
+    }
+
+    public function book(){
+        return view('front.book');
+    }
+}
